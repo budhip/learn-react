e6b0d590ae2e02571740c6ef5461f7366f66e9fb
budhip <ryo.bunraku@yahoo.com>
2017-08-19 19:56:40 +0700

    lesson 7 - nesting component

--- src/app/index.js
+++ src/app/index.js
@@ -12,7 +12,7 @@ var TodoComponent = React.createClass({
     var todos = this.state.todos;
     todos = todos.map(function(item, index){
       return (
-        <li>{item}</li>
+        <TodoItem item={item} key={index} />
       );
     });
     return(
@@ -24,6 +24,19 @@ var TodoComponent = React.createClass({
       </div>
     );
   }//render
+});
+
+//create TodoItem component
+var TodoItem = React.createClass({
+  render: function() {
+    return (
+      <li>
+        <div className="todo-item">
+          <span className="item-name">{this.props.item}</span>
+        </div>
+      </li>
+    );
+  }
 });
 
 //put component into html page
